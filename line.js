4060f9206dbdd2281fbf9de1950af30795be39d3
Ankit jaiswal <itsmeankit.5262@gmail.com>
2023-01-03 23:28:17 +0530

    Day 1

---
+++ line.js
@@ -0,0 +1,31 @@
+Day 1
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+var name="Akhil";
+console.log(name);
+var name="Ramesh";
+console.log(name);
+var name="Fatima";
+console.log(name);
+
+var name="Akhil";
+var age=25;
+
+console.log(name,age);
+console.log(typeof(name),typeof(age));
+
+var name="Mohit";
+var school="Swarna Bharti school";
+var grade="10th";
+var section="A";
+var roll_no=15468;
+var marks=["English: 80","Maths: 70","Science: 85"];
+
+console.log("##################################");
+console.log(name);
+console.log(grade);
+console.log(section);
+console.log(roll_no);
+console.log(marks);
+console.log("####################################");
